8292de15aa375144168a9c9269eb53550d356850
jeeva <jeevaprakashdr@gmail.com>
2026-05-31 22:29:31 +0200

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -3,17 +3,25 @@ use std::io::{self, Write};
 use std::io::{BufReader, Read};
 
 fn main() {
-    let mut buf = [0u8; 512];
+    let mut user_input = [0u8; 512];
     let mut reader = BufReader::new(io::stdin());
 
     loop {
         print!("$ ");
         io::stdout().flush().unwrap();
 
-        let bytes_len = reader.read(&mut buf).unwrap();
-        println!(
-            "{}: command not found",
-            String::from_utf8(buf[..bytes_len - 1].to_vec()).unwrap()
-        );
+        let bytes_len = reader.read(&mut user_input).unwrap();
+        let cmd = &user_input[..bytes_len - 1];
+        match cmd {
+            b"exit" => {
+                return;
+            }
+            _ => {
+                println!(
+                    "{}: command not found",
+                    String::from_utf8(user_input[..bytes_len - 1].to_vec()).unwrap()
+                )
+            }
+        }
     }
 }
