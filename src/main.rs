6610cccd99f5568ff7b92b729c600eaee48439a6
jeeva <jeevaprakashdr@gmail.com>
2026-05-31 22:11:02 +0200

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,19 @@
-use std::io::Read;
 #[allow(unused_imports)]
 use std::io::{self, Write};
+use std::io::{BufReader, Read};
 
 fn main() {
-    print!("$ ");
-    io::stdout().flush().unwrap();
+    let mut buf = [0u8; 512];
+    let mut reader = BufReader::new(io::stdin());
 
-    let mut reader = io::BufReader::new(io::stdin());
-    let mut buf = [0; 512];
-    let bytes_len = reader.read(&mut buf).unwrap();
-    println!(
-        "{}: command not found",
-        String::from_utf8(buf[..bytes_len - 1].to_vec()).unwrap()
-    );
+    loop {
+        print!("$ ");
+        io::stdout().flush().unwrap();
+
+        let bytes_len = reader.read(&mut buf).unwrap();
+        println!(
+            "{}: command not found",
+            String::from_utf8(buf[..bytes_len - 1].to_vec()).unwrap()
+        );
+    }
 }
