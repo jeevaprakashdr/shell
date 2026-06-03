c2233eb9dda491868585090a6c72ac3ad0f01671
jeeva <jeevaprakashdr@gmail.com>
2026-06-03 10:27:49 +0200

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -10,30 +10,22 @@ fn main() {
     loop {
         cli.write("$ ");
         let (cmd, args) = cli.read().parse();
-        match &cmd[..] {
+        match cmd.as_slice() {
             b"exit" => {
                 return;
             }
             b"echo" => {
-                // println!("{}", String::from_utf8_lossy(&args));
                 cli.write_line(&String::from_utf8_lossy(&args));
             }
-            b"type" => match args {
-                val if val == "exit".as_bytes()
-                    || val == "echo".as_bytes()
-                    || val == "type".as_bytes() =>
-                {
-                    cli.write_line(&format!(
-                        "{} is a shell builtin",
-                        String::from_utf8(val).unwrap()
-                    ));
-                    // println!("{} is a shell builtin", String::from_utf8(val).unwrap());
-                }
-                _ => {
-                    // println!("{}: not found", String::from_utf8(args).unwrap());
-                    cli.write_line(&format!("{}: not found", String::from_utf8(args).unwrap()));
-                }
-            },
+            b"type" => {
+                let output = std::process::Command::new("sh")
+                    .arg("-c")
+                    .arg(format!("type {}", String::from_utf8(args.clone()).unwrap()))
+                    .output()
+                    .unwrap();
+
+                cli.write(&String::from_utf8(output.stdout).unwrap());
+            }
             _ => {
                 cli.write_line(&format!(
                     "{}: command not found",
